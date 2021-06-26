35da63447f9a0381665cca15287559830744b686
Allen Keettikkal (alk423) <alk423@mail.usask.ca>
2021-06-26 17:44:00 -0600

    Create an instance of a panel in GameFrame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,5 +3,11 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class GameFrame {
+public class GameFrame extends JFrame{
+
+    GamePanel panel = new GamePanel();
+
+    GameFrame() {
+
+    }
 }

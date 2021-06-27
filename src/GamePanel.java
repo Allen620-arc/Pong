f4e8111c4eae602fbc691cd771c8d02755c41ed3
Allen Keettikkal (alk423) <alk423@mail.usask.ca>
2021-06-27 12:21:46 -0600

    Declares all variables for GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,9 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
-public class GamePanel {
+public class GamePanel extends JPanel implements Runnable{
+
+    static final int GAME_WIDTH = 1000;
+    static final int GAME_HEIGHT = 600;
+    static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+    static final int BALL_DIAMETER = 20;
+    static final int PADDLE_WIDTH = 25;
+    static final int PADDLE_HEIGHT = 25;
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Random random;
+    Paddle paddle1;
+    Paddle paddle2;
+    Ball ball;
+    Score score;
 
     GamePanel() {
 

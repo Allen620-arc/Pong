74a5d38c3fea9a99b6654f635ee26fbe99dfd171
Allen Keettikkal (alk423) <alk423@mail.usask.ca>
2021-06-27 12:28:52 -0600

    Declared all variables for Paddle

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,9 +1,12 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
-public class Paddle {
+public class Paddle extends Rectangle{
+
+    int id;
+    int yVelocity;
 
     Paddle() {
 
